7480c9d913389750bf7a8bed6bac64213a734d17
PaladinTyrion <paladintyrion@gmail.com>
2014-08-23 19:52:46 +0800

    modify the indentation

--- test.hpp
+++ test.hpp
@@ -20,38 +20,38 @@ enum location
 const std::multimap< location, std::pair< location, size_t > > & map( )
 {
 	static std::multimap< location, std::pair< location, size_t > > ret( []()
-	{
-		std::multimap< location, std::pair< location, size_t > > ret;
-		auto add_edge = [&]( location a, location b, size_t cost )
-		{
+			{
+			std::multimap< location, std::pair< location, size_t > > ret;
+			auto add_edge = [&]( location a, location b, size_t cost )
+			{
 			ret.insert( { a, { b, cost } } );
 			ret.insert( { b, { a, cost } } );
-		};
-		add_edge( Sibiu, Rimnicu_Vilcea, 80 );
-		add_edge( Rimnicu_Vilcea, Pitesti, 97 );
-		add_edge( Pitesti, Bucharest, 101 );
-		add_edge( Bucharest, Fagaras, 211 );
-		add_edge( Fagaras, Sibiu, 99 );
-		add_edge( Oradea, Zerind, 71 );
-		add_edge( Zerind, Arad, 75 );
-		add_edge( Arad, Timisoara, 118 );
-		add_edge( Oradea, Sibiu, 151 );
-		add_edge( Arad, Sibiu, 140 );
-		add_edge( Timisoara, Lugoj, 111 );
-		add_edge( Lugoj, Mehadia, 70 );
-		add_edge( Mehadia, Drobeta, 75 );
-		add_edge( Drobeta, Craivoa, 120 );
-		add_edge( Craivoa, Rimnicu_Vilcea, 146 );
-		add_edge( Craivoa, Pitesti, 138 );
-		add_edge( Bucharest, Giurgiu, 90 );
-		add_edge( Bucharest, Urziceni, 85 );
-		add_edge( Urziceni, Hirsova, 98 );
-		add_edge( Hirsova, Eforie, 86 );
-		add_edge( Urziceni, Vaslui, 142 );
-		add_edge( Vaslui, Iasi, 92 );
-		add_edge( Iasi, Neamt, 87 );
-		return ret;
-	}( ) );
+			};
+			add_edge( Sibiu, Rimnicu_Vilcea, 80 );
+			add_edge( Rimnicu_Vilcea, Pitesti, 97 );
+			add_edge( Pitesti, Bucharest, 101 );
+			add_edge( Bucharest, Fagaras, 211 );
+			add_edge( Fagaras, Sibiu, 99 );
+			add_edge( Oradea, Zerind, 71 );
+			add_edge( Zerind, Arad, 75 );
+			add_edge( Arad, Timisoara, 118 );
+			add_edge( Oradea, Sibiu, 151 );
+			add_edge( Arad, Sibiu, 140 );
+			add_edge( Timisoara, Lugoj, 111 );
+			add_edge( Lugoj, Mehadia, 70 );
+			add_edge( Mehadia, Drobeta, 75 );
+			add_edge( Drobeta, Craivoa, 120 );
+			add_edge( Craivoa, Rimnicu_Vilcea, 146 );
+			add_edge( Craivoa, Pitesti, 138 );
+			add_edge( Bucharest, Giurgiu, 90 );
+			add_edge( Bucharest, Urziceni, 85 );
+			add_edge( Urziceni, Hirsova, 98 );
+			add_edge( Hirsova, Eforie, 86 );
+			add_edge( Urziceni, Vaslui, 142 );
+			add_edge( Vaslui, Iasi, 92 );
+			add_edge( Iasi, Neamt, 87 );
+			return ret;
+			}( ) );
 	return ret;
 }
 
@@ -61,16 +61,16 @@ BOOST_AUTO_TEST_CASE( BFS_TEST )
 	auto sf = []( const std::pair< location, std::pair< location, size_t > > & pp ){ return pp.second.first; };
 	std::list< location > res;
 	breadth_first_search(
-						Sibiu,
-						[&](location l, const auto & it)
-						{
-							auto tem = map( ).equal_range( l );
-							std::copy( boost::make_transform_iterator( tem.first, sf ),
-							boost::make_transform_iterator( tem.second, sf ),
-							it );
-						},
-						[](location l){ return l == Bucharest; },
-						std::back_inserter( res ) );
+			Sibiu,
+			[&](location l, const auto & it)
+			{
+			auto tem = map( ).equal_range( l );
+			std::copy( boost::make_transform_iterator( tem.first, sf ),
+				boost::make_transform_iterator( tem.second, sf ),
+				it );
+			},
+			[](location l){ return l == Bucharest; },
+			std::back_inserter( res ) );
 	BOOST_CHECK_EQUAL( res, std::list< location >( { Sibiu, Fagaras, Bucharest } ) );
 }
 
@@ -79,17 +79,17 @@ BOOST_AUTO_TEST_CASE( UCS_TEST )
 	auto sf = []( const std::pair< location, std::pair< location, size_t > > & pp ){ return pp.second; };
 	std::list< location > res;
 	recursive_uniform_cost_search(
-				Sibiu,
-				0,
-				10000,
-				[&]( location l, auto it )
-				{
-					auto tem = map( ).equal_range( l );
-					std::copy( boost::make_transform_iterator( tem.first, sf ), boost::make_transform_iterator( tem.second, sf ), it );
-				},
-				[](location l){ return l == Bucharest; },
-				[]( auto cost ){ BOOST_CHECK_EQUAL( cost, 278 ); },
-				std::back_inserter( res ) );
+			Sibiu,
+			0,
+			10000,
+			[&]( location l, auto it )
+			{
+			auto tem = map( ).equal_range( l );
+			std::copy( boost::make_transform_iterator( tem.first, sf ), boost::make_transform_iterator( tem.second, sf ), it );
+			},
+			[](location l){ return l == Bucharest; },
+			[]( auto cost ){ BOOST_CHECK_EQUAL( cost, 278 ); },
+			std::back_inserter( res ) );
 	BOOST_CHECK_EQUAL( res, std::list< location >( { Sibiu, Rimnicu_Vilcea, Pitesti, Bucharest } ) );
 }
 
@@ -98,17 +98,17 @@ BOOST_AUTO_TEST_CASE( DFS )
 	auto sf = []( const std::pair< location, std::pair< location, size_t > > & pp ){ return pp.second.first; };
 	std::list< location > res;
 	depth_first_search( Sibiu,
-						[&](location l, const auto & it)
-						{
-							auto tem = map( ).equal_range( l );
-							std::copy(
-										boost::make_transform_iterator( tem.first, sf ),
-										boost::make_transform_iterator( tem.second, sf ),
-										it );
-						},
-						[](location, location ret){return ret;},
-						[](location l){ return l == Bucharest; },
-						std::back_inserter( res ) );
+			[&](location l, const auto & it)
+			{
+			auto tem = map( ).equal_range( l );
+			std::copy(
+				boost::make_transform_iterator( tem.first, sf ),
+				boost::make_transform_iterator( tem.second, sf ),
+				it );
+			},
+			[](location, location ret){return ret;},
+			[](location l){ return l == Bucharest; },
+			std::back_inserter( res ) );
 	BOOST_CHECK_EQUAL( res.back( ), Bucharest );
 }
 
@@ -117,18 +117,18 @@ BOOST_AUTO_TEST_CASE( IDDFS )
 	auto sf = []( const std::pair< location, std::pair< location, size_t > > & pp ){ return pp.second.first; };
 	std::list< location > res;
 	iterative_deepening_depth_first_search(
-				Sibiu,
-				[&](location l, const auto & it)
-				{
-					auto tem = map( ).equal_range( l );
-					std::copy(
-								boost::make_transform_iterator( tem.first, sf ),
-								boost::make_transform_iterator( tem.second, sf ),
-								it );
-				},
-				[](location, location ret){ return ret; },
-				[](location l){ return l == Bucharest; },
-				std::back_inserter( res ) );
+			Sibiu,
+			[&](location l, const auto & it)
+			{
+			auto tem = map( ).equal_range( l );
+			std::copy(
+				boost::make_transform_iterator( tem.first, sf ),
+				boost::make_transform_iterator( tem.second, sf ),
+				it );
+			},
+			[](location, location ret){ return ret; },
+			[](location l){ return l == Bucharest; },
+			std::back_inserter( res ) );
 	BOOST_CHECK_EQUAL( res, std::list< location >( { Fagaras, Bucharest } ) );
 }
 
@@ -139,8 +139,8 @@ BOOST_AUTO_TEST_CASE( BBFS )
 	{
 		auto tem = map( ).equal_range( l );
 		std::copy( boost::make_transform_iterator( tem.first, sf ),
-				   boost::make_transform_iterator( tem.second, sf ),
-				   it );
+				boost::make_transform_iterator( tem.second, sf ),
+				it );
 	};
 	std::list< location > res;
 	biderectional_breadth_first_search( Lugoj, Fagaras, expand, expand, std::back_inserter( res ) );
@@ -165,38 +165,38 @@ BOOST_AUTO_TEST_CASE( AOS )
 		std::vector< vacum_world > vec;
 		switch ( a )
 		{
-		case left:
-			vec.push_back( v );
-			{
-				vacum_world n( v );
-				n.is_left = true;
-				vec.push_back( n );
-			}
-			break;
-		case right:
-			vec.push_back( v );
-			{
-				vacum_world n( v );
-				n.is_left = false;
-				vec.push_back( n );
-			}
-			break;
-		case suck:
-			{
-				vacum_world n( v );
-				( n.is_left ? n.left_clean : n.right_clean ) = true;
-				vec.push_back( n );
-			}
-			break;
+			case left:
+				vec.push_back( v );
+				{
+					vacum_world n( v );
+					n.is_left = true;
+					vec.push_back( n );
+				}
+				break;
+			case right:
+				vec.push_back( v );
+				{
+					vacum_world n( v );
+					n.is_left = false;
+					vec.push_back( n );
+				}
+				break;
+			case suck:
+				{
+					vacum_world n( v );
+					( n.is_left ? n.left_clean : n.right_clean ) = true;
+					vec.push_back( n );
+				}
+				break;
 		}
 		std::copy( vec.begin( ), vec.end( ), it );
 	};
 	auto cleaned = [](const vacum_world & v){return v.left_clean && v.right_clean; };
 	auto res = and_or_search< action >(
-				vacum_world( true, false, false ),
-				cleaned,
-				[&](const vacum_world &, auto it){std::copy( act.begin( ), act.end( ), it );},
-				possibility );
+			vacum_world( true, false, false ),
+			cleaned,
+			[&](const vacum_world &, auto it){std::copy( act.begin( ), act.end( ), it );},
+			possibility );
 	BOOST_CHECK( res );
 	table_driven_agent< vacum_world, action > cleaner( * res );
 	auto test = [&](const auto & self,const vacum_world & v, std::set< vacum_world > & history)
@@ -230,45 +230,45 @@ BOOST_AUTO_TEST_CASE( CSP )
 	std::list< size_t > carry { 0, 1 };
 	std::list< local_constraint< var, size_t > > con;
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { O, R, C1 } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ass[0] * 2 == ass[1] + ass[2] * 10; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { O, R, C1 } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ass[0] * 2 == ass[1] + ass[2] * 10; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { O, R } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ( ass[0] * 2 - ass[1] ) % 10 == 0; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { O, R } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ( ass[0] * 2 - ass[1] ) % 10 == 0; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { C1, W, U, C2 } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ass[0] + ass[1] * 2 == ass[2] + 10 * ass[3]; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { C1, W, U, C2 } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ass[0] + ass[1] * 2 == ass[2] + 10 * ass[3]; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { C1, W, U } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ( ass[0] + ass[1] * 2 - ass[2] ) % 10 == 0; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { C1, W, U } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ( ass[0] + ass[1] * 2 - ass[2] ) % 10 == 0; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { C2, T, O, C3 } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ass[0] + ass[1] * 2 == ass[2] + 10 * ass[3]; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { C2, T, O, C3 } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ass[0] + ass[1] * 2 == ass[2] + 10 * ass[3]; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { C2, T, O } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ( ass[0] + ass[1] * 2 - ass[2] ) % 10 == 0; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { C2, T, O } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ( ass[0] + ass[1] * 2 - ass[2] ) % 10 == 0; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { C3, F } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ass[0] == ass[1]; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { C3, F } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ass[0] == ass[1]; } ) );
 	con.push_back(
-				make_local_constraint< var, size_t >(
-					std::vector< var >( { F } ),
-					[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-					{ return ass[0] != 0; } ) );
+			make_local_constraint< var, size_t >(
+				std::vector< var >( { F } ),
+				[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+				{ return ass[0] != 0; } ) );
 	for ( var i : { F, T, U, W, R, O } )
 	{
 		for ( var j : { F, T, U, W, R, O } )
@@ -276,18 +276,18 @@ BOOST_AUTO_TEST_CASE( CSP )
 			if ( i > j )
 			{
 				con.push_back(
-							make_local_constraint< var, size_t >(
-								std::vector< var >( { i, j } ),
-								[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
-								{ return ass[0] != ass[1]; } ) );
+						make_local_constraint< var, size_t >(
+							std::vector< var >( { i, j } ),
+							[&]( const std::vector< std::reference_wrapper< const size_t > > & ass )
+							{ return ass[0] != ass[1]; } ) );
 			}
 		}
 	}
 	backtracking_search(
-				std::map< var, std::list< size_t > >( { {F, digits},{O, digits},{U, digits},{R, digits},{T, digits},{W, digits},{C1, carry},{C2, carry},{C3, carry} } ),
-				4,
-				con,
-				std::back_inserter( result ) );
+			std::map< var, std::list< size_t > >( { {F, digits},{O, digits},{U, digits},{R, digits},{T, digits},{W, digits},{C1, carry},{C2, carry},{C3, carry} } ),
+			4,
+			con,
+			std::back_inserter( result ) );
 	BOOST_CHECK_EQUAL( result.size( ), 7 );
 	for ( const std::map< var, size_t > & ass : result )
 	{
