27a7df2bb473fb75d7428bc6596f5d9fd89b60c0
jerrycheng567 <1292652198@qq.com>
2014-08-26 18:59:53 +0000

    Implement part of wumpus_agent.

--- wumpus_world.hpp
+++ wumpus_world.hpp
@@ -1,11 +1,15 @@
 #ifndef WUMPUS_WORLD_HPP
 #define WUMPUS_WORLD_HPP
+#include <list>
 #include <cstddef>
 #include <set>
 #include <utility>
 #include <vector>
 #include <algorithm>
 #include "../propositional_calculus_prover/CNF.hpp"
+#include  "../propositional_calculus_prover/proposition.hpp"
+#include <boost/function_output_iterator.hpp>
+#include <boost/optional/optional.hpp>
 enum direction { north, east, south, west };
 template< size_t x, size_t y >
 struct wumpus_world
@@ -194,28 +198,110 @@ struct wumpus_world
 	}
 };
 
+struct knoweldge_base
+{
+	propositional_calculus::CNF data;
+	void insert( const propositional_calculus::clause & c ) { data.data.insert( c ); }
+	void insert( const propositional_calculus::proposition & p )
+	{
+		propositional_calculus::CNF cnf( propositional_calculus::to_CNF( p ) );
+		std::copy( cnf.data.begin( ), cnf.data.end( ), std::inserter( data.data, data.data.begin( ) ) );
+	}
+};
+
 template< size_t x, size_t y >
 struct wumpus_agent
 {
 	typedef wumpus_world< x, y > world;
-	propositional_calculus::CNF knoweldge_base;
+	knoweldge_base kb;
 	const world & env;
-	typename world::action operator( )( )
+	typedef propositional_calculus::proposition propsition;
+	typedef typename world::action action;
+	typedef typename world::coordinate coordinate;
+	void update_previous_action( const action & act ) { }
+	std::set< coordinate > un_visited;
+	std::list< action > plan;
+	typename world::action operator( )( const boost::optional< action > & act )
 	{
-		knoweldge_base.data.insert( { literal( gold( ), env.agent.current_sense.glitter ) } );
-
+		un_visited.erase( env.agent.position );
+		if ( act ) { update_previous_action( act ); }
+		if ( env.agent.current_sense.scream )
+		{
+			std::set< literal > tem;
+			for ( size_t i = 0; i < x; ++i )
+			{
+				for ( size_t j = 0; j < y; ++j )
+				{ tem.insert( literal( wumpus( coordinate( i, j ) ), false ) ); }
+			}
+			kb.insert( tem );
+		}
+		else if( current_time != 0 )
+		{
+			for ( size_t i = 0; i < x; ++i )
+			{
+				for ( size_t j = 0; j < y; ++j )
+				{ kb.insert( propositional_calculus::make_iff( proposition( wumpus( coordinate( i, j ) ), propsition( old_wumpus( coordinate( i, j ) ) ) ) ) ); }
+			}
+		}
+		if ( env.agent.current_sense.breeze )
+		{
+			clause tem;
+			env.surronding_squares( boost::make_function_output_iterator( [&]( const coordinate & c ){ tem.data.insert( literal( pit( c ), true ) ); } ) );
+			kb.insert( tem );
+		}
+		else { env.surronding_squares( boost::make_function_output_iterator( [&]( const coordinate & c ){ kb.insert( { literal( pit( c ), false ) } ); } ) ); }
+		if ( env.agent.current_sense.stench )
+		{
+			clause tem;
+			env.surronding_squares( boost::make_function_output_iterator( [&]( const coordinate & c ){ tem.data.insert( literal( wumpus( c ), true ) ); } ) );
+			kb.insert( tem );
+		}
+		else { env.surronding_squares( boost::make_function_output_iterator( [&]( const coordinate & c ){ kb.insert( { literal( wumpus( c ), false ) } ); } ) ); }
+		kb.insert( { literal( wumpus( env.agent.position ), false ) } );
+		kb.insert( { literal( pit( env.agent.position ), false ) } );
+		if ( env.agent.current_sense.glitter ) { return world::pickup_gold; }
+		if ( ! plan.empty( ) && env.agent.carrying_gold )
+		{
+			//plan a route out
+		}
+		if ( ! plan.empty( ) )
+		{
+			//plan to go to a safe square to explore
+		}
+		if ( ! plan.empty( ) && env.agent.have_arrow )
+		{
+			//plan to shoot wumpus
+		}
+		if ( ! plan.empty( ) )
+		{
+			//plan to go to a possibly safe square to explore
+		}
+		if ( ! plan.empty( ) )
+		{
+			//plan a route out
+		}
+		if ( ! plan.empty( ) )
+		{
+			action ret = plan.front( );
+			plan.pop_front( );
+			return ret;
+		}
 	}
 	size_t current_time;
 	typedef propositional_calculus::clause clause;
 	typedef propositional_calculus::literal literal;
-	typedef typename world::coordinate coordinate;
 	std::string square( const coordinate & c ) const { return std::to_string( c.first ) + "," + std::to_string( c.second ); }
 	std::string time( ) const { return std::string( current_time ); }
+	std::string old_time( ) const
+	{
+		assert( current_time != 0 );
+		return std::string( current_time - 1 );
+	}
 	std::string square_time( const coordinate & c ) const { return square( c ) + "," + time( ); }
+	std::string old_square_time( const coordinate & c ) const { return square( c ) + "," + old_time( ); }
 	std::string pit( const coordinate & c ) const { return "P" + square( c ); }
-	std::string gold( const coordinate & c ) const { return "G" + square_time( c ); }
 	std::string wumpus( const coordinate & c ) const { return "W" + square_time( c ); }
-	std::string arrow( ) const { return "A" + time( ); }
+	std::string old_wumpus( const coordinate & c ) const { return "W" + old_square_time( c ); }
 	wumpus_agent( const world & w ) : env( w ), current_time( 0 )
 	{
 		assert( w.is_end( ) == false );
@@ -227,9 +313,9 @@ struct wumpus_agent
 				for ( size_t i = 0; i < x; ++i )
 				{
 					for ( size_t j = 0; j < y; ++j )
-					{ tem.insert( literal( make( typename world::coordinate( i, j ) ), true ) ); }
+					{ tem.insert( literal( make( coordinate( i, j ) ), true ) ); }
 				}
-				knoweldge_base.data.insert( clause( std::move( tem ) ) );
+				kb.insert( clause( std::move( tem ) ) );
 			}
 			{
 				for ( size_t i = 0; i < x; ++i )
@@ -243,18 +329,22 @@ struct wumpus_agent
 								std::set< literal > tem;
 								tem.insert( literal( make( i, j ), false ) );
 								tem.insert( literal( make( k, l ), false ) );
-								knoweldge_base.data.insert( clause( std::move( tem ) ) );
+								kb.insert( clause( std::move( tem ) ) );
 							}
 						}
 					}
 				}
 			}
 		};
+		for ( size_t i = 0; i < x; ++i )
+		{
+			for ( size_t j = 0; j < y; ++j )
+			{ un_visited.insert( ( coordinate( i, j ) ) ); }
+		}
 		have_unique( [this]( const typename world::coordinate & c ){ return gold( c ); } );
 		have_unique( [this]( const typename world::coordinate & c ){ return wumpus( c ); } );
-		knoweldge_base.data.insert( { literal( wumpus( w.agent.position ), false ) } );
-		knoweldge_base.data.insert( { literal( pit( w.agent.position ), false ) } );
-		knoweldge_base.data.insert( { literal( arrow( ), true ) } );
+		kb.insert( { literal( wumpus( w.agent.position ), false ) } );
+		kb.insert( { literal( pit( w.agent.position ), false ) } );
 	}
 };
 
